bef41c7b55b364540e342583b79b39fa9e46b5e7
Cristianbenz <contactocbenitez@gmail.com>
2023-05-19 12:35:53 -0300

    add tailwind, new projects and update resume

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#031926",
+        secondary: "#a5e6ba",
+      }
+    },
+  },
+  plugins: [],
+}
+
